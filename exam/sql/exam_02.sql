0b4600de41f521fa9ce2254bf397538841eacc60
Konstantin Botalov <konstantin@botalov.pro>
2024-11-12 17:09:50 +0300

    Выполнено задание 2 промежуточной аттестации 2.

--- exam/sql/exam_02.sql
+++ exam/sql/exam_02.sql
@@ -20,3 +20,26 @@ BEGIN
     END IF;
 END $$;
 
+/*
+   Задание 2.
+   Через анонимный блок посчитать сколько четных и нечетных чисел в некотором промежутке чисел,
+   левая граница и правая граница диапазона должны быть как переменные
+*/
+DO $$
+DECLARE
+    min_range INTEGER := 2;  -- левая граница диапазона
+    max_range INTEGER := 4; -- правая граница диапазона
+    even_count INTEGER := 0; -- счетчик четных чисел
+    odd_count INTEGER := 0;  -- счетчик нечетных чисел
+    i INTEGER;               -- переменная для итерации
+BEGIN
+    FOR i IN min_range..max_range LOOP
+        IF i % 2 = 0 THEN
+            even_count := even_count + 1;  -- увеличиваем счетчик четных чисел
+        ELSE
+            odd_count := odd_count + 1;    -- увеличиваем счетчик нечетных чисел
+        END IF;
+    END LOOP;
+
+    RAISE NOTICE 'В промежутке от % до % всего % нечетных чисел и % четных', min_range, max_range, odd_count, even_count;
+END $$;
