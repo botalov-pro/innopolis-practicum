c7e1055b40831b97ec35bfd42c1adc06f6824b3d
Konstantin Botalov <konstantin@botalov.pro>
2024-11-14 13:37:11 +0300

    Добавлено преобразование типов для столбца release_date итогового проекта (c TEXT на DATE).

--- exam/sql/exam_final.sql
+++ exam/sql/exam_final.sql
@@ -7,3 +7,29 @@ CREATE DATABASE sales;
 /* Проверяем, что создалась наша база данных */
 SELECT datname FROM pg_database
 WHERE datname='sales';
+
+/*
+    Приведение типов
+    Создаем временный столбец для хранения преобразованных данных
+ */
+ALTER TABLE innopolis ADD COLUMN release_date_new DATE;
+
+/*
+    Перенос данных
+    Скопируем данные из старого столбца в новый, преобразуя данные с помощью функции TO_DATE
+ */
+UPDATE innopolis
+SET release_date_new = TO_DATE(release_date, 'YYYY-MM-DD')
+WHERE release_date IS NOT NULL;
+
+/*
+    Удаление старого столбца
+    Удаляем старый столбец
+ */
+ALTER TABLE innopolis DROP COLUMN release_date;
+
+/*
+    Переименование нового столбца
+    Переименуем временный столбец в исходное имя
+ */
+ALTER TABLE innopolis RENAME COLUMN release_date_new TO release_date;
